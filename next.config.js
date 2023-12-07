c7084136b15047ff263c241a531a6a23a39607ce
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2023-12-07 21:43:30 +0500

    Update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
       domains: ['*', 'scontent.flhe3-1.fna.fbcdn.net', 'images.unsplash.com', 'naturals.pk', 'cdn.shopify.com']
-    },
+    }
   };
   
   module.exports = nextConfig;
